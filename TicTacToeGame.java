36f99d13311ba5c9ea4bc4fe10dbf6dead5404c2
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-23 16:01:03 +0530

    Added: User Input and Checking for valid user input

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	
+	static Scanner sc = new Scanner(System.in);
 	private static char[] board = new char[10];
 	private static char userOption;
 	private static char computerOption;
+	private static int indexNumber;
 	
 	//UseCase 1 -Creating a board and initializing
 	public static void createBoard() 
@@ -24,7 +26,6 @@ public class TicTacToeGame {
 	 */
 	public static void chooseOption()
 	{
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter if you want to choose X or O: ");
 		userOption = sc.next().charAt(0); 
 		if(userOption=='X' || userOption=='x')
@@ -40,19 +41,38 @@ public class TicTacToeGame {
 			System.out.println("Player Team: " + userOption);
 			System.out.println("Computer Team: " + computerOption);
 		}
-		sc.close();
 	}
 	
-	//Method to display the board
+	/* UseCase 3 - Method to display the board
+	 * For loop helps in displaying the available spaces for the user to make a move
+	 */
 	public static void showBoard()
 	{
 		System.out.println("\n------------- ");
-		for(int i = 1 ; i<10 ; i+=3)
+		for(int i = 1 ; i< board.length ; i+=3)
 		{
 			System.out.println("| " + board[i] + " | " + board[i+1] + " | " + board[i+2] + " |");
 			System.out.println("------------- ");
 		}
 		
+	}
+	
+	/*
+	 * UseCase 4 - Ability of the user to make desired location in the board
+	 * In this function, we ask the user the input as to where he wants to place his X or O
+	 * @param indexNumber is used for the user to enter his input number.
+	 * The 'if' condition checks for a valid index number.
+	 */
+	public static void userIndexValue()
+	{
+		
+		System.out.println("Enter an index number where you want to place your input: ");
+		indexNumber = sc.nextInt(); 
+		if(indexNumber < 1 || indexNumber > 9)
+		{
+			System.out.println("Please enter a valid index number from 1-9. ");
+		}
+		sc.close();
 	}
 	
 	public static void main(String[] args) {
@@ -61,6 +81,7 @@ public class TicTacToeGame {
 		createBoard();		
 		chooseOption();
 		showBoard();
+		userIndexValue();
 	}
 
 }
