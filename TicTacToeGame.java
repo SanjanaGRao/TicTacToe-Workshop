68077e2ecbce3c5d20e932eda72e9280f5a799e5
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-23 10:01:04 +0530

    Modified: Tic Tac Toe - Creation of a Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,14 +2,13 @@ package TicTacToeWorkshop;
 
 public class TicTacToeGame {
 	
-	private static int length = 10;
 	private static char[] board = new char[10];
 	
 	//UseCase 1 -Creating a board and initializing
 	public static void createBoard() 
 	{
 		
-		for(int i = 1; i<length ; i++)
+		for(int i = 1; i<board.length ; i++)
 		{
 			board[i] = ' ';			
 		}
